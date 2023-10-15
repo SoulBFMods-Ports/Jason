171ba3b942382777b8a334410b8dd3f2a9d6a209
Carlos737392 <97768361+Carlos737392@users.noreply.github.com>
2023-10-15 13:47:59 -0300

    Delete assets/preload/data/relentless/script1.lua

--- assets/preload/data/relentless/script1.lua
+++
@@ -1,9 +0,0 @@
-local allowCountdown = false
-function onStartCountdown()
-	if not allowCountdown and not seenCutscene then --Block the first countdown
-		startVideo('jason cutscene phase 1 end');
-		allowCountdown = true;
-		return Function_Stop;
-	end
-	return Function_Continue;
-end
